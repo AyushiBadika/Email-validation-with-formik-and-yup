cd306ca0cc959cb01c011f0568c42c32496d1451
AyushiBadika <badikaayushi@gmail.com>
2024-08-08 16:36:40 +0530

    updated schema

--- src/schema/index.jsx
+++ src/schema/index.jsx
@@ -2,7 +2,10 @@ import * as Yup from "yup";
 
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(25).required("Please enter name"),
-  email: Yup.string().email().required("Please enter email"),
+  email: Yup.string()
+    .email()
+    .required("Please enter email")
+    .oneOf(["ayushi@gmail.com", null], "Email must be ayushi@gmail.com"),
   password: Yup.string()
     .min(8)
     .required("Please enter password")
